c20bc4a6e08d447ef1e98f9fec9c4ecc90086baa
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-09-14 13:51:42 -0400

    collect restaurants information

--- Yelp/Data_Preprocessing/data_cleaning.py
+++ Yelp/Data_Preprocessing/data_cleaning.py
@@ -1,20 +1,66 @@
 import json
 import pandas as pd 
+import csv 
+import math
+import numpy as np 
 
-def test_1():
-    # file_business, file_checkin, file_review, file_tip, file_user = ['../Yelp Dataset/yelp_academic_dataset_business.json', 
-    # '../Yelp Dataset/yelp_academic_dataset_checkin.json', '../Yelp Dataset/yelp_academic_dataset_review.json', 
-    # '../Yelp Dataset/yelp_academic_dataset_tip.json', '../Yelp Dataset/yelp_academic_dataset_user.json']
+remove_b_tag = lambda x: x[1:] if type(x)==str and x[0] =='b' else (x if type(x)==float else x)
 
-    # with open(file_business, encoding='utf-8') as f:
-    #     df_business = pd.DataFrame(json.loads(line) for line in f)
+def remove_b_tag_from_csv(csv_file):
+    df = pd.read_csv(csv_file)
 
-    # print(df_business.columns.values)
-    # print(df_business.head(2))
+    for column in df:
+        df[column] = df[column].map(remove_b_tag)
 
-    df_business = pd.read_csv(r'../Yelp Dataset/csv/yelp_academic_dataset_business.csv')
-    print(df_business['attributes.RestaurantsReservations'])
+    return df
+    
+def write_dataframe_to_csv_file(filename, dataframe):
+    dataframe.to_csv(filename, encoding='utf-8', index=False)
+
+    return None
+
+def filter_business_by_category(dataframe, category):
+    #remove any rows that are blank as they will cause a problem when we use the apply function below
+    dataframe_wo_NAs = dataframe.dropna(subset=['categories'])
+    
+    df_filtered = dataframe_wo_NAs[dataframe_wo_NAs['categories'].apply(lambda x: True if category in x else False)]
+    
+    return df_filtered
+
+def select_certain_columns_in_dataframe(dataframe, list_of_columns):
+    dataframe_w_certain_columns = dataframe[list_of_columns]
+
+    return dataframe_w_certain_columns
+
+def rename_columns_in_dataframe(dataframe, original, new):
+    dataframe.rename(columns={original:new}, inplace=True)
+
+    return dataframe
 
 
 if __name__ == '__main__':
-    test_1()
+    ###Collecting restaurant business data###
+
+    #write_dataframe_to_csv_file('yelp_academic_dataset_user.csv', remove_b_tag_from_csv('../Yelp Dataset/csv/yelp_academic_dataset_user.csv'))
+    df = pd.read_csv(r'../Yelp Dataset/csv/yelp_academic_dataset_business.csv')
+    df_filtered = filter_business_by_category(df, 'Restaurants')
+    
+    #placing business_id first as we will want to use this index to join the review file
+    selected_columns = ['business_id','attributes.RestaurantsPriceRange2', 'attributes.RestaurantsGoodForGroups', 'city', 
+    'categories', 'attributes.GoodForMeal', 'attributes.RestaurantsAttire', 'attributes.HasTV', 'attributes.RestaurantsDelivery',
+    'attributes.NoiseLevel', 'attributes.RestaurantsTakeOut', 'attributes.GoodForKids', 'attributes.Open24Hours', 'attributes.BusinessParking',
+    'attributes.WiFi', 'state', 'attributes', 'is_open', 'attributes.Alcohol', 'stars', 'attributes.RestaurantsTableService',
+    'name', 'attributes.Ambience']
+
+    #the column for stars should really be avg_stars. renaming this column and reducing the number of columns in final dataframe
+    dataframe_w_certain_columns = rename_columns_in_dataframe(select_certain_columns_in_dataframe(df_filtered, selected_columns), 'stars', 'avg_stars')
+    
+    #get the unique business_id and then later collect only the reviews with these ids
+    #every id should be unique as this is the business file and each line is about one business
+    list_unique_business_id = dataframe_w_certain_columns.business_id.unique()
+
+    write_dataframe_to_csv_file('business_file_restaurants_certain_columns.csv', dataframe_w_certain_columns)
+
+    
+
+    
