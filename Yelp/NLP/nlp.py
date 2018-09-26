297b25e9c02041585ec9025dcb8174bd8d67cf28
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-09-26 18:30:12 -0400

    code before refactoring - combining nlp rep train test w sim reviews

--- Yelp/NLP/nlp.py
+++ Yelp/NLP/nlp.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import dask.dataframe as dd
+import dask 
 import matplotlib.pyplot as plt 
 from sklearn.metrics.pairwise import cosine_similarity
 #from sklearn.model_selection import train_test_split
@@ -33,16 +34,18 @@ def create_dataset(df, column):
     # outfile = open(filename, 'wb')
     # pickle.dump(documents, outfile)
     # outfile.close()
-    dataframe_review_unpacked = df[column].compute()
-    documents = [review for review in dataframe_review_unpacked.values]
-    print(documents[1])
-    #documents = [t for i,t in enumerate(df[column])]
+    
+    # #documents = [t for i,t in enumerate(df[column])]
     
     
-    # documents = df[column]
+    documents = df[column]
     # documents.compute()
     #print(documents[1])
     #print(type(documents.head(1)))
+
+    # dataframe_review_unpacked = df[column].compute()
+    # documents = [review for review in dataframe_review_unpacked.values]
+    # print(documents[1])
 
     return documents
 
@@ -55,19 +58,24 @@ def create_training_test_set(df):
     df_train, df_test = df.random_split([0.8, 0.2], random_state=2)
 
     documents_train = df_train['text']
-    documents_test = df_test['stars']
+    documents_test = df_test['text']
+    target_train = df_train['stars']
+    target_test = df_test['stars']
 
     gc.collect()
-    return documents_train, documents_test
+    return documents_train, documents_test, target_train, target_test
 
 @profile 
-def find_similar_reviews(dataset_train, dataset_test, vectors_train, n):
+def find_similar_reviews(vectorizer, dataset_train, dataset_test, vectors_train, n):
     # Draw an arbitrary review from test (unseen in training) documents# Draw a 
     #hardcoding this for now. using randint returned memoryerror
+    dataset_train, dataset_test = dask.compute(dataset_train, dataset_test)
+    dataset_train = [review for review in dataset_train.values]
+    dataset_test = [review for review in dataset_test.values]
     doc_test = dataset_test[1]
     #doc_test = dataset_test[np.random.randint(len(dataset_test))]
     # Transform the drawn review(s) to vector(s)
-    doc_test_vector = TfidfVectorizer(stop_words = 'english', max_features=5000).transform([doc_test]).toarray()
+    doc_test_vector = vectorizer.transform([doc_test]).toarray()
     # Calculate the similarity score(s) between vector(s) and training vectors
     similarity_scores = cosine_similarity(doc_test_vector, vectors_train.toarray())
     #search for n similar reviews
@@ -93,8 +101,10 @@ def transform_vectorizer_to_array(dataset_test):
 @profile 
 def create_nlp_rep_of_train_test(dataset_train, dataset_test):
     #change the dask dataframes to pandas dataframes
-    dataset_train = dataset_train.compute()
-    dataset_test = dataset_test.compute()
+    dataset_train, dataset_test = dask.compute(dataset_train, dataset_test)
+    #print(dataset_train._is_mixed_type)
+    dataset_train = [review for review in dataset_train.values]
+    dataset_test = [review for review in dataset_test.values]
 
     # Create TfidfVectorizer, and name it vectorizer
     vectorizer = TfidfVectorizer(stop_words = 'english', max_features=5000)
@@ -109,7 +119,7 @@ def create_nlp_rep_of_train_test(dataset_train, dataset_test):
     #vectors_test = process_vector_transform_parallel(dataset_test, transform_vectorizer_to_array, 10, 6, 0, vectorizer)
     vectors_test = vectorizer.transform(dataset_test).toarray() 
 
-    return vectors_train, words, vectors_test
+    return vectorizer, vectors_train, words, vectors_test
 
 @profile 
 def get_top_values(lst, n, labels):
@@ -161,10 +171,10 @@ if __name__ == '__main__':
     #documents = pickle.load(infile)
     
 
-    # dataset_train, dataset_test = create_training_test_set(df)
-    # df = None
-    # gc.collect()
-    # vectors_train, words, vectors_test = create_nlp_rep_of_train_test(dataset_train, dataset_test)
-    # doc_test, searched_result = find_similar_reviews(dataset_train, dataset_test, vectors_train, 5)
-    # print(doc_test)
-    # print(searched_result)
+    documents_train, documents_test, target_train, target_test = create_training_test_set(df)
+    df = None
+    gc.collect()
+    vectorizer, vectors_train, words, vectors_test = create_nlp_rep_of_train_test(documents_train, documents_test)
+    doc_test, searched_result = find_similar_reviews(vectorizer, documents_train, documents_test, vectors_train, 5)
+    print(doc_test)
+    print(searched_result)
