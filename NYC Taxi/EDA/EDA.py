29a716daa87044b3a6fbae41d67910a92c4ca060
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-09-30 15:59:22 -0400

    correlation analyses

--- NYC Taxi/EDA/EDA.py
+++ NYC Taxi/EDA/EDA.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.core.pylabtools import figsize
 import numpy as np
+import seaborn as sns
 
 
 df = pd.read_csv('../train.csv')
@@ -57,18 +58,40 @@ if __name__ == "__main__":
     #print(df.nlargest(10, 'trip_duration'))
 
     #shorten the dataframe by whether the trip duration column is within 3 standard deviations
-    df = df[np.abs(df.trip_duration-df.trip_duration.mean()) <= (3*df.trip_duration.std())]
+    #there are extreme trip duration values that severely distorts the histogram
+    #df = df[np.abs(df.trip_duration-df.trip_duration.mean()) <= (3*df.trip_duration.std())]
     
 
     # # Histogram of the Trip Duration
-    figsize(8, 8)
+    #figsize(8, 8)
     
-    plt.hist(df['trip_duration'].dropna(), bins = 1000, edgecolor = 'k')
-    plt.xlabel('trip_duration') 
-    plt.ylabel('Number of Trips')
-    plt.title('Trip Duration Distribution')
-    plt.tight_layout()
-    plt.savefig('histograms_of_trip_duration_within_3_std.png')
+    # plt.hist(df['trip_duration'].dropna(), bins = 100, edgecolor = 'k')
+    # plt.xlabel('trip_duration') 
+    # plt.ylabel('Number of Trips')
+    # plt.title('Trip Duration Distribution')
+    # plt.tight_layout()
+    # plt.savefig('histograms_of_trip_duration_within_3_std.png')
+
+    #let's look at trip distribution based on vendors
+    # df_vendor_1 = df[df['vendor_id'] == 1]
+    # df_vendor_2 = df[df['vendor_id'] == 2]
+
+
+    # plt.hist(df_vendor_1['trip_duration'].dropna(), bins = 100, edgecolor = 'k')
+    # plt.xlabel('trip_duration') 
+    # plt.ylabel('Number of Trips')
+    # plt.title('Trip Duration Distribution for Vendor 1')
+    # plt.tight_layout()
+    # plt.savefig('Vendor_1_histograms_of_trip_duration_within_3_std.png')
+    
+    # plt.hist(df_vendor_2['trip_duration'].dropna(), bins = 100, edgecolor = 'k')
+    # plt.xlabel('trip_duration') 
+    # plt.ylabel('Number of Trips')
+    # plt.title('Trip Duration Distribution for Vendor 2')
+    # plt.tight_layout()
+    # plt.savefig('Vendor_2_histograms_of_trip_duration_within_3_std.png')
+
+
 
     #look at datatype of each column
     #df.info().to_csv('datatype_desc.csv')
@@ -78,3 +101,42 @@ if __name__ == "__main__":
 
     #break histogram into buckets as we have some extreme values, distorting the histogram to look like one column
     
+
+    #density plot on passengar count. may tell us if passengar count can predict trip duration
+    #print(df['passenger_count'].nunique()) 10 unique passenger_count values
+
+    #print(df['passenger_count'].unique())
+    
+    # df = df[np.abs(df.trip_duration-df.trip_duration.mean()) <= (3*df.trip_duration.std())]
+    # list_passenger_count_unique = df['passenger_count'].unique()
+
+
+    # # Plot of distribution of scores for passenger categories
+    # figsize(12, 10)
+
+    # # Plot each passenger count
+    # for passenger_count in list_passenger_count_unique:
+    #     if passenger_count == 0 or passenger_count == 1 or passenger_count == 2:
+    #         # Select the passenger count type
+    #         subset = df[df['passenger_count'] == passenger_count]
+        
+    #         # Density plot of passenger_count
+    #         sns.kdeplot(subset['trip_duration'].dropna(),
+    #                 label = passenger_count, shade = False, alpha = 0.8)
+        
+    # # label the plot
+    # plt.xlabel('Trip Duration', size = 20); plt.ylabel('Density', size = 20)
+    # plt.title('Density_Plot_of_Trip_Duration_by_PassCount-0-2', size = 28)
+    # plt.savefig('Density_Plot_of_Trip_Duration_by_PassCount-0-2.png')
+
+    """Find all correlations with trip_duration and sort"""
+    #one hot encode the flag column first
+    df = pd.get_dummies(df, columns=["store_and_fwd_flag"])
+    #feature engineering with datetime objects
+    
+    
+    
+    correlations_data = df.corr()['trip_duration'].sort_values()
+
+    correlations_data.to_csv("correlation_data.csv")
+
