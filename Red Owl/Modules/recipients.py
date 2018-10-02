e8c960b388b8c1e221036accff87e2342017df17
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-02 08:54:08 -0400

    refactored code

--- Red Owl/Modules/recipients.py
+++ Red Owl/Modules/recipients.py
@@ -1,66 +1,77 @@
-import pandas as pd 
-from collections import defaultdict, Counter
+"""Function performing analyses of recipients"""
 
+from collections import defaultdict
+import pandas as pd
 
-dictionary_for_msgs_received_by_recipient = defaultdict(int)
+def cnt_msgs_recvd_by_each_recip(df_parse_recip):
+    """move the recipients from the dataframe into a list first
+    to make it easier to work with. use dict to count msgs received
+    by recipient"""
+    msgs_rcvd_by_recipient = defaultdict(int)
+    #storing recipients into a list so that we do not have to
+    #loop through a dataframe which is time costly
+    for column in df_parse_recip:
+        tmp_list = df_parse_recip[column].tolist()
+        tmp_list = [str(word) for word in tmp_list if str(word) != 'nan']
+        #mirror image of the dictionary for number of messages the senders sent.
+        #e.g. if john adams was in 2 rows of the recipients column,
+        #john adams received a message twice
+        for recipient in tmp_list:
+            msgs_rcvd_by_recipient[recipient] += 1
 
-list_of_recipients = []
-add_to_list = lambda x: list_of_recipients.append(str(x)) if str(x) != 'nan' else None
-
-def count_messages_received_by_each_recipient(dataframe_from_parse_recipients):
-    #storing recipients into a list so that we do not have to loop through a dataframe which is time costly
-    for column in dataframe_from_parse_recipients:
-        dataframe_from_parse_recipients[column].map(add_to_list)
-
-    #mirror image of the dictionary for number of messages the senders sent. e.g. if john adams was in 2 rows of the recipients column, john adams received a message twice
-    for recipient in list_of_recipients:
-        dictionary_for_msgs_received_by_recipient[recipient] +=1
-
-    return dictionary_for_msgs_received_by_recipient
-
-
-parse_recipient_by_pipe = lambda x: pd.Series([i for i in reversed(str(x).split('|'))])
+    return msgs_rcvd_by_recipient
 
 def parse_recipients(dataframe):
-    parsed_recipients_dataframe = dataframe['recipients'].apply(parse_recipient_by_pipe)
+    """multiple recipients in one cell, separated by pipe.
+    parse each recipient into individual cell"""
 
-    return parsed_recipients_dataframe
-  
+    parse_recip_by_pipe = lambda x: pd.Series([i for i in reversed(str(x).split('|'))])
+    parsed_recip_df = dataframe['recipients'].apply(parse_recip_by_pipe)
 
-def convert_cell_value_false_if_not_top_sender(cell_value, list_of_top_five_senders):
-    if cell_value not in list_of_top_five_senders:
+    return parsed_recip_df
+
+def convert_false_if_not_tsender(cell_value, top_five_senders):
+    """make cell value false if it is not a top sender.
+    allows later analysis to quickly skip any cell with false values.
+    this function placed in this module because want to know how
+    many msgs top senders received"""
+
+    if cell_value not in top_five_senders:
         return False
-    else:
-        return cell_value
+    return cell_value
 
 def create_column_headers(number_of_columns):
+    """creates a column header when recipient column is parsed.
+    makes the column header based on string value rather than number,
+    which may get shifted as we are deleting the column in a later analysis"""
+
     column_indexes = []
-    #index to be used on dataframe so that we delete unnecessary columns based on name rather than number, which may get shifted as we are deleting the column
     for i in range(number_of_columns):
         column_indexes.append('a'+str(i))
-    
     return column_indexes
 
-def collect_columns_n_rows_top_senders_in_recipient(dataframe_from_parse_recipients, list_of_top_five_senders):
-    list_of_column_n_rows_w_top_senders = []
+def coll_cols_rows_tsenders_recip(df_parse_recip, top_five_senders):
+    """collects the cell value (column and row indexes), where the top five
+    senders are located in the parsed recipient dataframe. modifies the parsed recipient
+    dataframe to delete columns where the rows equals False"""
+
+    col_n_rows_w_top_senders = []
     rows_of_interest = []
 
-    number_of_columns = len(dataframe_from_parse_recipients.columns)
-    
-    #index to be used on dataframe so that we delete unnecessary columns based on name rather than number, which may get shifted as we are deleting the column
-    column_indexes = dataframe_from_parse_recipients.columns.values
+    #index to be used on dataframe so that we delete unnecessary columns based on name rather
+    #than number, which may get shifted as we are deleting the column
+    column_indexes = df_parse_recip.columns.values
 
-    for column in dataframe_from_parse_recipients:
-        dataframe_from_parse_recipients[column] = dataframe_from_parse_recipients[column].apply(convert_cell_value_false_if_not_top_sender, args=(list_of_top_five_senders,))
-        
+    for column in df_parse_recip:
+        df_parse_recip[column] = df_parse_recip[column].apply(convert_false_if_not_tsender, args=(top_five_senders,))
+
     for index in column_indexes:
-        if any(dataframe_from_parse_recipients[index]):
+        if any(df_parse_recip[index]):
             #get the row index value where the top senders occur
-            rows_of_interest = dataframe_from_parse_recipients.index[dataframe_from_parse_recipients[index] != False].tolist()
-            list_of_column_n_rows_w_top_senders.append((index, rows_of_interest))
+            rows_of_interest = df_parse_recip.index[df_parse_recip[index] != False].tolist()
+            col_n_rows_w_top_senders.append((index, rows_of_interest))
         else:
             #delete column that is full of False values
-            del dataframe_from_parse_recipients[index]
-    
-    return dataframe_from_parse_recipients, list_of_column_n_rows_w_top_senders
-   
+            del df_parse_recip[index]
+
+    return df_parse_recip, col_n_rows_w_top_senders
