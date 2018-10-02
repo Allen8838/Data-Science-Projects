e2ec44ec38a7b47bed3b0046727e90f361eb19f1
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-02 08:54:25 -0400

    refactored code

--- Red Owl/Modules/senders.py
+++ Red Owl/Modules/senders.py
@@ -1,38 +1,40 @@
-import pandas as pd 
+"""Functions on performing analyses on senders"""
+
 from collections import defaultdict, Counter
 
 
-dictionary_for_number_msgs_sent_by_sender = defaultdict(int)
-dictionary_senders_list_of_time = defaultdict(list)
-dictionary_senders_number_msgs_per_time = defaultdict(list)
+def create_dict_senders_num_msgs(df):
+    """Create a dictionary of the number of messages a sender sent,
+    and a dictionary that counts the number of messages a sender sent
+    on a certain date"""
+    senders = []
+    add_senders_ls = lambda x: senders.append(str(x)) if x != '' and x != 'sender' else None
+    time_ls = []
+    add_time_ls = lambda x: time_ls.append(x) if x != '' and x != 'time' else None
 
-list_of_senders = []
-list_of_time = []
-list_of_senders_time = []
+    #putting the df column into a list through map as
+    # looping through each row in the df is time consuming
+    df['sender'].map(add_senders_ls)
+    df['time'].map(add_time_ls)
 
-add_to_list_of_senders = lambda x: list_of_senders.append(str(x)) if x != '' and x != 'sender' else None
-add_to_list_of_time = lambda x: list_of_time.append(x) if x != '' and x != 'time' else None
+    senders_time = zip(senders, time_ls)
 
-def create_dict_of_senders_n_count_num_msgs(dataframe):
-    #putting the dataframe column into a list through map as looping through each row in the dataframe is time consuming
-    dataframe['sender'].map(add_to_list_of_senders)
-    dataframe['time'].map(add_to_list_of_time)
-
-    list_of_senders_time = zip(list_of_senders, list_of_time)
-
-    unzipped_list_of_senders_time = list(list_of_senders_time)
+    unzipped_senders_time = list(senders_time)
 
     #create a "dictionary" of counts instances of the same sender time pair
-    #resulting dictionary will look something like e.g. ('trnews tr', '2002-11-30'): 1, ('hussey eudoramail', '2002-12-21'): 1}
-    count_senders_date_messages = Counter(tuple_pair for tuple_pair in unzipped_list_of_senders_time)
+    #resulting dictionary will look something like e.g. ('trnews tr', '2002-11-30'):
+    # 1, ('hussey eudoramail', '2002-12-21'): 1}
+    count_senders_date_messages = Counter(tuple_pair for tuple_pair in unzipped_senders_time)
 
+    dict_num_msgs_sender_sent = defaultdict(int)
     #count the number of messages per sender
-    for sender in list_of_senders:
-        dictionary_for_number_msgs_sent_by_sender[sender] += 1
+    for sender in senders:
+        dict_num_msgs_sender_sent[sender] += 1
 
+    dict_senders_num_msgs_per_time = defaultdict(list)
     #Need this dictionary for question 2
     for sender_time in count_senders_date_messages:
-        dictionary_senders_number_msgs_per_time[sender_time[0]].append((count_senders_date_messages[sender_time], sender_time[1]))
+        dict_senders_num_msgs_per_time[sender_time[0]].append((count_senders_date_messages[sender_time], sender_time[1]))
 
     
-    return dictionary_for_number_msgs_sent_by_sender, dictionary_senders_number_msgs_per_time, unzipped_list_of_senders_time
+    return dict_num_msgs_sender_sent, dict_senders_num_msgs_per_time, unzipped_senders_time
