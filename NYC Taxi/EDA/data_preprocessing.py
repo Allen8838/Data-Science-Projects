93a28ff9832e160bb49b21a94b0f9cd29c5ee830
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-03 13:42:19 -0400

    separated some code from EDA to this module

---
+++ NYC Taxi/EDA/data_preprocessing.py
@@ -0,0 +1,91 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from IPython.core.pylabtools import figsize
+import numpy as np
+import seaborn as sns
+
+def get_desc_stats(df, csv_name, png_name):
+    df.describe().to_csv(name)
+    df.hist()
+    plt.tight_layout()
+    plt.savefig(png_name)
+
+# Function to calculate missing values by column
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+
+def haversine_array(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.    
+
+    """
+    R = 6371.0  # radius of the earth in km
+
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = R * c
+    return km
+
+def dummy_manhattan_distance(lat1, lng1, lat2, lng2):
+    a = haversine_array(lat1, lng1, lat1, lng2)
+    b = haversine_array(lat1, lng1, lat2, lng1)
+    return a + b
+
+def bearing_array(lat1, lng1, lat2, lng2):
+    AVG_EARTH_RADIUS = 6371  # in km
+    lng_delta_rad = np.radians(lng2 - lng1)
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+    y = np.sin(lng_delta_rad) * np.cos(lat2)
+    x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(lng_delta_rad)
+    return np.degrees(np.arctan2(y, x))
+
+def convert_datetime_n_round(df, column, min):
+    df[column] =  pd.to_datetime(df[column])
+    
+    return df[column].dt.round(min)
+
+
+def split_date_time(df, column):
+    #using normalize even though it gives us 0:00 time, but the resulting column is a datetime object, which allows us to further process
+    #for day of week
+ 
+    date_var = df[column].dt.normalize()
+    time_var = df[column].dt.time
+
+    return date_var, time_var
+
+def find_day_of_week(df, column):
+    return df[column].dt.day_name()
