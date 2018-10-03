d90f9ad56289257d9934d85e4df2f8544f38ebcf
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-03 13:41:52 -0400

    refactored code

--- NYC Taxi/EDA/EDA.py
+++ NYC Taxi/EDA/EDA.py
@@ -3,59 +3,87 @@ import matplotlib.pyplot as plt
 from IPython.core.pylabtools import figsize
 import numpy as np
 import seaborn as sns
+from data_preprocessing import get_desc_stats, ,missing_values_table, haversine_array, dummy_manhattan_distance, bearing_array, convert_datetime_n_round, split_date_time, find_day_of_week
 
+def create_cols_distances():
+    #create a column for haversine distance
+    df['distance'] = haversine_array(df['pickup_longitude'], df['pickup_latitude'], df['dropoff_longitude'], df['dropoff_latitude'])
 
-df = pd.read_csv('../train.csv')
+    df['manhattan_distance'] = dummy_manhattan_distance(df['pickup_longitude'], df['pickup_latitude'], df['dropoff_longitude'], df['dropoff_latitude'])
 
+    df['bearing'] = bearing_array(df['pickup_longitude'], df['pickup_latitude'], df['dropoff_longitude'], df['dropoff_latitude'])
 
-# Function to calculate missing values by column
-def missing_values_table(df):
-        # Total missing values
-        mis_val = df.isnull().sum()
-        
-        # Percentage of missing values
-        mis_val_percent = 100 * df.isnull().sum() / len(df)
-        
-        # Make a table with the results
-        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-        
-        # Rename the columns
-        mis_val_table_ren_columns = mis_val_table.rename(
-        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
-        
-        # Sort the table by percentage of missing descending
-        mis_val_table_ren_columns = mis_val_table_ren_columns[
-            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
-        '% of Total Values', ascending=False).round(1)
-        
-        # Print some summary information
-        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
-            "There are " + str(mis_val_table_ren_columns.shape[0]) +
-              " columns that have missing values.")
-        
-        # Return the dataframe with missing information
-        return mis_val_table_ren_columns
+    return df
+
+def remove_outliers(df, trip_dur_max, trip_dur_min, dist_min):
+    #delete select rows where the data is anomalous 
+    df = df[df['trip_duration'].values <= trip_dur_max] #any rows where the trip is over 10 hours (3600 seconds)
+    df = df[df['trip_duration'].values >= trip_dur_min] #at least 5 min
+    df = df[df['distance'].values >= dist_min] #at least 0.5 km or 6 blocks
+
+    return df
+
+def create_avg_speed_cols():
+    #create speed column. this should be correlated with the day component
+    #and may give additional insight
+    df['avg_speed_haversine'] = 1000*df['distance'].values/df['trip_duration']
+    df['avg_speed_manhattan'] = 1000*df['manhattan_distance'].values/df['trip_duration']
+
+    return df 
 
 
 
-
-
-if __name__ == "__main__":
-    # print(list(df.columns.values))
-    # df.describe().to_csv('descriptive statistics.csv')
-    # df.hist()
-    # plt.tight_layout()
-    # plt.savefig('histograms_of_columns.png')
-    #print(df.T.apply(lambda x: x.nunique(), axis=1))
-    
+if __name__ == "__main__":    
+    df = pd.read_csv('../train.csv')    
+    get_desc_stats(df, 'descriptive statistics.csv', 'histograms_of_columns.png')
     #see which columns has missing values and look at how much missing values are there
-    #missing_values_table(df)
+    missing_values_table(df)
 
-    #delete the row value with the outlier trip duration as it is skewing the trip duration histogram dramatically
-    #row_to_delete = df['trip_duration'].idxmax()
-    #df.drop(df.index[row_to_delete])
+    df = create_cols_distances()
 
-    #print(df.nlargest(10, 'trip_duration'))
+    #one hot encode the flag column first
+    df = pd.get_dummies(df, columns=["store_and_fwd_flag"])
+
+    df = remove_outliers(df, 3600, 300, 0.5)
+
+    df = create_avg_speed_cols()
+
+    #convert pickup_date and dropoff_date to datetime objects
+    #round both pickup and dropoff time to the latest 15 min before applying sin and cos functions to them
+    #assuming that hours will matter, not minutes and want to get to the closest hour
+    
+    df['pickup_datetime_rounded_15_min'] = convert_datetime_n_round(df, 'pickup_datetime', '15min')
+    df['dropoff_datetime_rounded_15_min'] = convert_datetime_n_round(df, 'dropoff_datetime', '15min')
+
+    df['rounded_pickup_time_hour'] = pd.to_datetime(df['pickup_datetime_rounded_15_min']).dt.hour 
+    df['rounded_dropoff_time_hour'] = pd.to_datetime(df['dropoff_datetime_rounded_15_min']).dt.hour 
+
+    df['pickup_time_hour_sin'] = np.sin(2 * np.pi * df['rounded_pickup_time_hour']/23.0)
+    df['pickup_time_hour_cos'] = np.cos(2 * np.pi * df['rounded_pickup_time_hour']/23.0)
+
+    df['dropoff_time_hour'] = np.sin(2 * np.pi * df['rounded_dropoff_time_hour']/23.0)
+    df['rounded_dropoff_time_hour'] = np.cos(2 * np.pi * df['rounded_dropoff_time_hour']/23.0)
+
+    #split datetime between dates and time
+    #using normalize even though it gives us 0:00 time, but the resulting column is a datetime object, which allows us to further process
+    #for day of week
+    df['pickup_date'], df['pickup_time'] = split_date_time(df, 'pickup_datetime_rounded_15_min')
+    df['dropoff_date'], df['dropoff_time'] = split_date_time(df, 'dropoff_datetime_rounded_15_min')
+     
+    #create day of the week for both pickup date and dropoff dates
+    df['pickup_day_of_week'] = find_day_of_week(df, 'pickup_datetime_rounded_15_min') 
+
+    df['dropoff_day_of_week'] = find_day_of_week(df, 'dropoff_datetime_rounded_15_min')
+
+    #one hot encode day of the week for both pickup and dropoff
+    df = pd.get_dummies(df, columns=['pickup_day_of_week', 'dropoff_day_of_week'])
+
+    df.head(10000).to_csv('dataframe_w_datetime_feature_engineering.csv')
+    correlations_data = df.corr()['trip_duration'].sort_values()
+
+    correlations_data.to_csv("correlation_data.csv")
+
+
 
     #shorten the dataframe by whether the trip duration column is within 3 standard deviations
     #there are extreme trip duration values that severely distorts the histogram
@@ -91,24 +119,8 @@ if __name__ == "__main__":
     # plt.tight_layout()
     # plt.savefig('Vendor_2_histograms_of_trip_duration_within_3_std.png')
 
-
-
-    #look at datatype of each column
-    #df.info().to_csv('datatype_desc.csv')
-
-    #get a small subset of the data for viewing
-    #df.head(10000).to_csv('first_10000_dataset_records.csv')
-
     #break histogram into buckets as we have some extreme values, distorting the histogram to look like one column
     
-
-    #density plot on passengar count. may tell us if passengar count can predict trip duration
-    #print(df['passenger_count'].nunique()) 10 unique passenger_count values
-
-    #print(df['passenger_count'].unique())
-    
-    # df = df[np.abs(df.trip_duration-df.trip_duration.mean()) <= (3*df.trip_duration.std())]
-    # list_passenger_count_unique = df['passenger_count'].unique()
 
 
     # # Plot of distribution of scores for passenger categories
@@ -132,59 +144,13 @@ if __name__ == "__main__":
     """Find all correlations with trip_duration and sort"""
     """Feature Engineering with Dates"""
     
-    
-    # #one hot encode the flag column first
-    df = pd.get_dummies(df, columns=["store_and_fwd_flag"])
 
-    #delete any rows where the trip is over 10 hours (3600 seconds)
-    df = df[df['trip_duration'].values <= 3600]
+ 
 
     print(df.shape)
     #feature engineering with datetime objects
     #split data from time for pickup and dropoff datetime object
     
-    #convert pickup_date and dropoff_date to datetime objects
-    df['pickup_datetime'] =  pd.to_datetime(df['pickup_datetime'])
-    df['dropoff_datetime'] =  pd.to_datetime(df['dropoff_datetime'])
-
-    #round both pickup and dropoff time to the latest 15 min before applying sin and cos functions to them
-    #assuming that hours will matter, not minutes and want to get to the closest hour
-    df['pickup_datetime_rounded_15_min'] = df['pickup_datetime'].dt.round('15min')
-    df['dropoff_datetime_rounded_15_min'] = df['dropoff_datetime'].dt.round('15min')
-
-    df['rounded_pickup_time_hour'] = pd.to_datetime(df['pickup_datetime_rounded_15_min']).dt.hour 
-    df['rounded_dropoff_time_hour'] = pd.to_datetime(df['dropoff_datetime_rounded_15_min']).dt.hour 
-
-    df['pickup_time_hour_sin'] = np.sin(2 * np.pi * df['rounded_pickup_time_hour']/23.0)
-    df['pickup_time_hour_cos'] = np.cos(2 * np.pi * df['rounded_pickup_time_hour']/23.0)
-
-    df['dropoff_time_hour'] = np.sin(2 * np.pi * df['rounded_dropoff_time_hour']/23.0)
-    df['rounded_dropoff_time_hour'] = np.cos(2 * np.pi * df['rounded_dropoff_time_hour']/23.0)
-
-    #split datetime between dates and time
-    #using normalize even though it gives us 0:00 time, but the resulting column is a datetime object, which allows us to further process
-    #for day of week
-    df['pickup_date'] = df['pickup_datetime_rounded_15_min'].dt.normalize()
-    df['pickup_time'] = df['pickup_datetime_rounded_15_min'].dt.time
-
-    
-    df['dropoff_date'] = df['dropoff_datetime_rounded_15_min'].dt.normalize()
-    df['dropoff_time'] = df['dropoff_datetime_rounded_15_min'].dt.time
-    
-    #create day of the week for both pickup date and dropoff dates
-    df['pickup_day_of_week'] = df['pickup_datetime_rounded_15_min'].dt.day_name()
-
-    df['dropoff_day_of_week'] = df['dropoff_datetime_rounded_15_min'].dt.day_name()
-
-    #compute 
-
-    #one hot encode day of the week for both pickup and dropoff
-    df = pd.get_dummies(df, columns=['pickup_day_of_week', 'dropoff_day_of_week'])
-
-    df.head(10000).to_csv('dataframe_w_datetime_feature_engineering.csv')
-    correlations_data = df.corr()['trip_duration'].sort_values()
-
-    correlations_data.to_csv("correlation_data.csv")
 
     #create pair plot 
     #data_pairplot = sns.pairplot(df)
