732bb9174f3ce49923aa26632023f6aab8306ca7
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-07 22:07:31 -0400

    refactored code

--- NYC Taxi/Modeling/check.py
+++ NYC Taxi/Modeling/check.py
@@ -1,6 +1,7 @@
+"""sanity checks to make sure that training and testing distribution matches"""
+
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime as dt
 import seaborn as sns
 import pandas as pd
 
@@ -15,26 +16,24 @@ def check_valid_test_dist(model, dtest, test, dvalid):
     print('Valid prediction mean: %.3f' % ypred.mean())
     print('Test prediction mean: %.3f' % ytest.mean())
 
-    fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True)
+    _, ax = plt.subplots(nrows=2, sharex=True, sharey=True)
     sns.distplot(ypred, ax=ax[0], color='blue', label='validation prediction')
     sns.distplot(ytest, ax=ax[1], color='green', label='test prediction')
     ax[0].legend(loc=0)
     ax[1].legend(loc=0)
     plt.savefig('Validation and Test Distributions.png')
-
-    t1 = dt.datetime.now()
     
 
-def check_train_test_dist(train, test):
-    feature_stats = pd.DataFrame({'feature': feature_names})
-    feature_stats.loc[:, 'train_mean'] = np.nanmean(train[feature_names].values, axis=0).round(4)
-    feature_stats.loc[:, 'test_mean'] = np.nanmean(test[feature_names].values, axis=0).round(4)
+def check_train_test_dist(train, test, features_used):
+    feature_stats = pd.DataFrame({'feature': features_used})
+    feature_stats.loc[:, 'train_mean'] = np.nanmean(train[features_used].values, axis=0).round(4)
+    feature_stats.loc[:, 'test_mean'] = np.nanmean(test[features_used].values, axis=0).round(4)
 
-    feature_stats.loc[:, 'train_std'] = np.nanstd(train[feature_names].values, axis=0).round(4)
-    feature_stats.loc[:, 'test_std'] = np.nanstd(test[feature_names].values, axis=0).round(4)
+    feature_stats.loc[:, 'train_std'] = np.nanstd(train[features_used].values, axis=0).round(4)
+    feature_stats.loc[:, 'test_std'] = np.nanstd(test[features_used].values, axis=0).round(4)
 
-    feature_stats.loc[:, 'train_nan'] = np.mean(np.isnan(train[feature_names].values), axis=0).round(3)
-    feature_stats.loc[:, 'test_nan'] = np.mean(np.isnan(test[feature_names].values), axis=0).round(3)
+    feature_stats.loc[:, 'train_nan'] = np.mean(np.isnan(train[features_used].values), axis=0).round(3)
+    feature_stats.loc[:, 'test_nan'] = np.mean(np.isnan(test[features_used].values), axis=0).round(3)
 
     feature_stats.loc[:, 'train_test_mean_diff'] = np.abs(feature_stats['train_mean'] - feature_stats['test_mean']) / np.abs(feature_stats['train_std'] + feature_stats['test_std'])  * 2
     feature_stats.loc[:, 'train_test_nan_diff'] = np.abs(feature_stats['train_nan'] - feature_stats['test_nan'])
