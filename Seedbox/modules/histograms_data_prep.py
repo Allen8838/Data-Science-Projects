a6ddf362ff52b653491ac997bc81bcdc154bd83b
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-09 10:54:30 -0400

    refactored code

--- Seedbox/modules/histograms_data_prep.py
+++ Seedbox/modules/histograms_data_prep.py
@@ -1,45 +1,34 @@
-#general overview for code below is:
-#find the transaction amounts associated with the test group, put it into a list and return it
-#find the transaction amounts associated with the control group, put it into a list and return it 
+"""
+general overview for code below is:
+find the transaction amounts associated with the test group, put it into a list and return it
+find the transaction amounts associated with the control group, put it into a list and return it
+"""
 
-from collections import defaultdict 
-from itertools import repeat
+def get_ls_trans_amts_tst_con(df, usr_grp, trans_amt):
+    trans_amt_tst_grp = []
+    trans_amt_ctrl_grp = []
 
-test_group_list = []
-trans_amt_test_group_list = []
+    create_trans_tst = lambda x: trans_amt_tst_grp.append(x) if int(x) != 0.00 else None
+    create_trans_ctrl = lambda x: trans_amt_ctrl_grp.append(x) if int(x) != 0.00 else None
 
-control_group_list = []
-trans_amt_control_group_list = []
+    #creating this to add to df. this will then be multiplied against the
+    #transaction amount so that we can get a column of only transaction amounts
+    #associated with control groups
+    indic_col_ctrl = lambda x: int(x)+1 if int(x) == 0 else 0
 
-create_trans_test_list = lambda x: trans_amt_test_group_list.append(x) if int(x) != 0.00 else None
-create_trans_control_list = lambda x: trans_amt_control_group_list.append(x) if int(x) != 0.00 else None 
-
-#creating this to add to dataframe. this will then be multiplied against the trnasaction amount so that we can get a column of only transaction amounts associated with control groups
-indicator_column_for_control = lambda x: int(x)+1 if int(x) == 0 else 0
-
-
-def get_lists_trans_amts_for_test_n_control(dataframe, user_group, transaction_amount):
-    #create two columns in the dataframe, based on whether the transaction amount is associated with test or control
-    #since our user group is only 1's and 0's, the code just below will only select the test group with a transaction amount
-    dataframe['trans_test_group'] = dataframe[user_group]*dataframe[transaction_amount] 
+    #create two columns in the df, based on whether the transaction amount
+    #is associated with test or control since our user group is only 1's and 0's, 
+    #the code just below will only select the test group with a transaction amount
+    df['trans_test_group'] = df[usr_grp]*df[trans_amt] 
     #creating an indicator column to flip the 1's and 0's, so that 1 indicates the presence of a control group
-    dataframe['one_control_zero_test_indicator_column'] = dataframe[user_group].map(indicator_column_for_control)
-    dataframe['trans_control_group'] = dataframe['one_control_zero_test_indicator_column']*dataframe[transaction_amount]
+    df['one_control_zero_test_indicator_column'] = df[usr_grp].map(indic_col_ctrl)
+    df['trans_control_group'] = df['one_control_zero_test_indicator_column']*df[trans_amt]
 
-    dataframe['trans_test_group'].map(create_trans_test_list)
-    dataframe['trans_control_group'].map(create_trans_control_list)
+    df['trans_test_group'].map(create_trans_tst)
+    df['trans_control_group'].map(create_trans_ctrl)
     
    
-    #reset the dataframe to the way it was so that it doesn't interfere with subsequent analyses
-    dataframe = dataframe
+    #reset the df to the way it was so that it doesn't interfere with subsequent analyses
+    df = df
 
-    return trans_amt_test_group_list, trans_amt_control_group_list 
-
-
-
-
-
-
-
-
-
+    return trans_amt_tst_grp, trans_amt_ctrl_grp
