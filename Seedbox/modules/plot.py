5d7d0869b09ce09d40476d61eafbc88f0e32c462
Allen8838 <35154803+Allen8838@users.noreply.github.com>
2018-10-10 13:37:17 -0400

    minor edits

--- Seedbox/modules/plot.py
+++ Seedbox/modules/plot.py
@@ -1,12 +1,14 @@
+"""
+create histograms
+"""
+
 import matplotlib.pyplot as plt
 
-
-
-def create_histograms(list_test_for_histogram, list_control_for_histogram):
-    plt.hist(list_test_for_histogram, 30, label='Test group', facecolor='green')
-    plt.hist(list_control_for_histogram, 30, label= 'Control group', facecolor='orange')
+def create_histograms(tst_histo, ctrl_histo):
+    plt.hist(tst_histo, 30, label='Test group', facecolor='green')
+    plt.hist(ctrl_histo, 30, label='Control group', facecolor='orange')
     plt.legend(loc='upper right')
     plt.xlabel('Transaction Amount')
     plt.ylabel('Number of instances')
     plt.title(r'Probability distributions of test and control group')
-    plt.show()
+    plt.savefig('Probability_distributions_of_test_and_control_group.png')
